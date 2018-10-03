a5121f901b7a2859475fecae5f7fdf93ce34e907
Mervyn Fung <mervynfung96@gmail.com>
2018-10-04 00:50:27 +0800

    ABc

--- js/userInfo.js
+++ js/userInfo.js
@@ -2,11 +2,9 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
         return firebase.database().ref('/users/' + user.uid).once('value').then(function(snapshot) {
-            var fullName = (snapshot.val() && snapshot.val().fullname) || 'Anonymous';
             var ic = (snapshot.val() && snapshot.val().ic) || 'Anonymous';
             var contactNum = (snapshot.val() && snapshot.val().contactNumber) || 'Anonymous';
             var email = user.email;
-            var address = (snapshot.val() && snapshot.val().address) || 'Anonymous';
             //alert(fullName + " " + ic + " "+ contactNum+ " "+ address);
 
             $("#fullName").text(fullName);
